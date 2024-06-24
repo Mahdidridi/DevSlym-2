19033ebbd0d34151911d7f2f2d24a4b48b2f3401
ayoub <ayoubaitihia@gmail.com>
2024-06-24 19:24:59 +0200

    v.1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::inertia('/test','Test');
+
+Route::inertia('/cong','Cong');
+
+Route::fallback(function () {
+    return Inertia::render('NotFound');
+});
+
